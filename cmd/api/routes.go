9a4c55b2017f9dda874bf3905457f0a60f153987
Tiago <tiago.seben.dev@gmail.com>
2024-07-15 00:25:47 -0300

    :sparkles: feat: Add posts crud

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,8 +5,15 @@ import "github.com/go-chi/chi/v5"
 func (app *application) routes() *chi.Mux {
 	router := chi.NewRouter()
 
-	router.Route("/v1", func(r chi.Router) {
+	router.Route("/api/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+			r.Get("/{id}", app.showPostHandler)
+			r.Get("/", app.listPostsHandler)
+			r.Put("/{id}", app.editPostHandler)
+			r.Delete("/{id}", app.deletePostHandler)
+		})
 	})
 
 	return router
